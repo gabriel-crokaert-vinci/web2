3e0e394f3929926364d4352b79fa1ae6afbbdd4d
gabriel-crokaert-vinci <gabriel.crokaert@student.vinci.be>
2023-09-25 14:01:03 +0200

    correction

--- exercices/1.1/app.js
+++ exercices/1.1/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var pizzaRouter = require('./routes/pizzas');
-var indexRouter = require('./routes/index');
+var filmsRouter = require('./routes/films');
 
 var app = express();
 
@@ -12,8 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/pizzas', pizzaRouter);
-app.use('/index', indexRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
